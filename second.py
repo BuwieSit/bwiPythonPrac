3b4e70b1b93eed067b4008aecd8e8de36aab9311
BuwieSit <santosbuhawie28@gmail.com>
2025-09-16 22:59:48 +0800

    number guessing game

--- second.py
+++ second.py
@@ -4,20 +4,41 @@ import random
 print("Number guessing game")
 while True:
 
-        min = input("Minimum number: ")
-        max = input("Maximum number: ")
+        min = int(input("Minimum number: "))
+        max = int(input("Maximum number: "))
         difficulty = input("ENABLE HARD MODE? (Decimal values) (y|n) ")
         print("Loading random number...")
         
         match difficulty:
             case "n":
-                numbers = int(random.randint(min, max))
+                number = random.randint(min, max)
             case "y":
-                numbers = float(random.random(min, max))
+                
+                number = float(random.uniform(min, max))
+                number = round(number, 2)
             case _:
                 print('Invalid. Restarting')
 
-        confirmation = input('Continue? (y/n): ').lower()
+        print("debug", number)
+        while True:
+            if type(number) == float:
+                answer = float(input("Guess it: "))
+            elif type(number) == int:
+                answer = int(input("Guess it: "))
+            else:
+                print("Type error.")
+                
+            if answer == number:
+                print("You guessed it right! The number was ",number)
+                break
+            elif answer < number:
+                print("Maybe higher?")
+            elif answer > number:
+                print("Maybe lower")
+            else:
+                print("Uhhh what?")
+            
+        confirmation = input('Want another session? (y/n): ').lower()
         if confirmation == 'y':
             continue
         elif confirmation == 'n':
